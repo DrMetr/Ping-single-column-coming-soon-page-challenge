95242e22a76905526d36ab9e83442a25d7691ae3
Daria M. <metronomedragon@gmail.com>
2025-04-01 16:19:34 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          //RedError: "hsl(0, 100%, 66%)",
+          Blue: "hsl(223, 87%, 63%)",
+          PaleBlue: "hsl(223, 100%, 88%)",
+          LightRed: "hsl(354, 100%, 66%)",
+          Gray: "hsl(0, 0%, 59%)",
+          DarkBlue: "hsl(209, 33%, 12%)"
+        },
+        fontFamily: {
+          LibreFranklin: ["Libre Franklin", "sans-serif"],
+        },
+      },
+      plugins: [],
+    },
+  };
